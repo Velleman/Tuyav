4e0f979bb3b49afa941488477f8651998b6817ea
velbn <8317833+velbn@users.noreply.github.com>
2020-02-07 11:12:39 +0100

    Update Example

--- examples/tuyav_example/tuyav_example.ino
+++ examples/tuyav_example/tuyav_example.ino
@@ -1,61 +1,93 @@
+/* 
+ *  --- TUYAV example program ---
+ *  v1.0 by BN&PSI @ Velleman NV - feb 2020
+ *  works with VMA354 Tuya IoT interface
+ *  
+ *  This example program shows you how to interface with the Tuya IoT interface board
+ *  for the full manual, please visit https://www.vellemanformakers.com/product/VMA354
+ *  
+ *  Connections:
+ *  VMA354    Arduino UNO     Arduino MEGA
+ *  VCC       5V              5V
+ *  GND       GND             GND
+ *  TX        RX (pin2)       RX1 (pin19)
+ *  RX        TX (pin3)       TX1 (pin18)
+ */
+
+
+//libraries
 #include "Tuyav.h"
 #include "global.h"
 
-
-#if defined(ARDUINO_AVR_UNO)
-//User can choose SoftwareSerial
-//you can choose your own pins
-SoftwareSerial mySerial(2,3);
-Tuyav tuyav(&mySerial);
-#else // Mega
-  //User can choose HardwareSerial: Serial1/Serial2/Serial3
-  // Serial1 is pin 18/19 on a Arduino Mega
-  Tuyav tuyav(&Serial1);
+//selection of Serial port
+#if defined(ARDUINO_AVR_UNO)    //Arduino UNO board: use SoftwareSerial with pins you select, see https://www.arduino.cc/en/Reference/softwareSerial
+SoftwareSerial mySWserial(2,3); //RX,TX (2 and 3 are recommended)
+Tuyav tuyav(&mySWserial);         
+#else                           //Arduino Mega board: User can choose HardwareSerial: Serial1/Serial2/Serial3
+  Tuyav tuyav(&Serial1);        //Serial1 is pin 18/19 on a Arduino Mega
 #endif
+
 //Initialize Time for updating Arbitrary Values
+unsigned long currentTime = 0;
 unsigned long previousTime = 0;
-bool isLampOn = false;
+int updateDelay = 3000;    //3 seconds by default. Min 1 second or you will overflow the serial communication!
 
-void setup() {
 
-  /*
-  *If user does not use a pin: set the pin as PIN_UNUSED
-  *PIN 18 is A5
-  */
+void setup() 
+{
+  //start serial for debugging
+  Serial.begin(9600);
+  Serial.println("Tuya Demo program");
+  
+  //define the TUYA pins
+  // There are 3 digital inputs, 3 analog inputs, 5 digital output and 3 analog outputs available
+  // If you do not use a pin, set the pin as PIN_UNUSED
+  tuyav.setDigitalInputs(4, 5, 6);                    //Set DigitalInputs
+  tuyav.setAnalogInputs(A0, A1, A2);                  //Set AnalogInputs
+  tuyav.setDigitalOutputs(13, 12, 8, 7, PIN_UNUSED);  //SetDigitalOutputs
+  tuyav.setAnalogOutputs(9, 10, 11);                  //Set AnalogOutputs (PWM digital pins)
 
-  tuyav.setDigitalInputs(8, 12, 18);        //Set DigitalInputs
-  tuyav.setAnalogInputs(A0, A1, A2);        //Set AnalogInputs
-  tuyav.setDigitalOutputs(13, 4, 5, 6, 7);  //SetDigitalOutputs
-  tuyav.setAnalogOutputs(9, 10, 11);               //Set AnalogOutputs
-
+  //init the chip
   tuyav.initialize();
-
 }
 
-void loop() {
+void loop() 
+{
   //Should be called continuously 
   tuyav.tuyaUpdate();
-  //User can send an Arbitrary value to the cloud calling "tuyav.sendUserStringValue(USER_VALUE_1/USER_VALUE_2/USER_VALUE_3, "UserStringValue")" and "tuyav.sendUserValue(USER_VALUE_4/, UserValue)"
+  
+  //check time
+  currentTime = millis();
 
-  int sensorValue1 = 10; //User define a value for USER_VALUE_4
-  int sensorValue2 = 200; //User can define a value for USER_VALUE_5
-  float gravity = 9.81f;
-  
-  
-    tuyav.setUserValue(AV1, "Test Application"); //User send string value "Kitchen" to the cloud
-    tuyav.setAV2("V1.1"); //User send string value "Living Room" to the cloud
-    String bedroomLamp = "Bedroom Lamp: ";
-    bedroomLamp += isLampOn ? "ON" : "OFF";
-    isLampOn = !isLampOn;
-    tuyav.setUserValue(AV3, bedroomLamp); //User send string value "Bedroom" to the cloud*/
-    tuyav.setUserValue(AV4, "Temp: 22°C"); //User send string value "Bedroom" to the cloud*/
-    tuyav.setUserValue(AV5, "ON"); //User send string value "Bedroom" to the cloud*/
-    tuyav.setUserValue(AV6, String(sensorValue2)); //User send string value "Bedroom" to the cloud*/
-    tuyav.setUserValue(AV7, String(gravity)); //User send string value "Bedroom" to the cloud*/
-    String message;
-    message = "Humidity :";
-    message += String(sensorValue1);
-    tuyav.setUserValue(AV8, "Elepsed Time: "); //User send string value "Bedroom" to the cloud*/
-    tuyav.setUserValue(AV9, String(millis())); //User send string value "Bedroom" to the cloud*/
-  
+  //timer: run the code below every time the updateDelay has passed
+  if(currentTime - previousTime > updateDelay)
+  {
+      //set arbitrary values (9 are available - read only in the app)
+      tuyav.setUserValue(AV1, "Test Application");
+      tuyav.setAV2("SW version");
+      tuyav.setAV3("V1.0");
+      String AV4msg = "Last update time:";
+      tuyav.setUserValue(AV4, AV4msg);
+      tuyav.setUserValue(AV5, String(currentTime - previousTime));
+      tuyav.setAV6("ms ago"); 
+      tuyav.setUserValue(AV7, "Time since boot:"); 
+      tuyav.setAV8(String(currentTime));
+      tuyav.setAV9("ms");
+
+      //check the state of a digital inputs (read only in the app)
+      Serial.print("State of DI2: "); Serial.println(tuyav.DIGITAL_IN[1]);
+      //check the state of a analog input (read only in the app)
+      int analogInCH2 = tuyav.ANALOG_IN[2];
+      Serial.print("State of AI3: "); Serial.println(analogInCH2);
+      //check the state of all digital outputs (can be set from the app)
+      for(int t = 0; t < 5; t++)
+      {
+        Serial.print("State of DO"); Serial.print(t+1); Serial.print(": "); Serial.println(tuyav.DIGITAL_OUT[t]);
+      }
+      //check the state of a analog output (can be set from the app)
+      Serial.print("State of AO1: "); Serial.println(tuyav.ANALOG_OUT[0]);
+      Serial.println("----");
+
+      previousTime = millis();   //now that everything has been done, the previous time is the current time (and not the var currentTime, because time has passed during this block of code)
+  }
 }
